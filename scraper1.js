97ec6fd9658d180afd322ffb20779ebe4e281178
lightdrk <mohitrawatssj4@gmail.com>
2023-09-06 15:49:47 +0530

    update

--- scraper1.js
+++ scraper1.js
@@ -4,17 +4,31 @@ const fs = require('fs');
 const progress = require('cli-progress');
 
 const bar1 = new progress.SingleBar({},progress.Presets.shades_classic);
+const currentDate = new Date();
+
+// Get the individual components of the date
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Add 1 because months are zero-indexed
+const day = currentDate.getDate().toString().padStart(2, '0');
+const year = currentDate.getFullYear().toString();
+
+// Combine the components into the desired format
+const formattedDate = `${month}${day}${year}`;
+
+console.log('Formatted Date:', formattedDate);
 
 let Email = '';
 let Pass = '';
-let stock_file = 'STonk.txt';
+let stock_file = 'new';
 
 function read(stock_file){
 	let words = fs.readFileSync(`./${stock_file}`,'utf-8');
 	return words.split('\n');
 }
 function convertToCSV(arr) {
-    return arr.map(row => row.join(',')).join('\n');
+		var quality = arr.find(item => item[0] === '1. Quality')[1];
+		var valuation = arr.find(item => item[0] === '2. Valuation')[1];
+		var priceTrend = arr.find(item => item[0] === '3. Price Trend')[1];
+		return `\n\n ${arr[0][0]} : {  ${quality}, ${valuation}, ${priceTrend}   }`
 }
 
 (async() =>{
@@ -47,132 +61,134 @@ function convertToCSV(arr) {
 	for (let stock of stock_list){
 		count++;
 		bar1.update(count);
-	// pop_search box
-		try{
-			await page.waitForNavigation({timeout: 2000});
-		}catch(error){
-			//console.log(error);\
-		}
-		try{	
-			await page.waitForSelector('#stocksearch-icon',{timeout:3000})
-			await page.click('#stocksearch-icon');
-		}catch (error){
-		//	console.log(error);
-			await page.click('#header-search-box');
-		}
-		await page.waitForSelector('input#popupsearch');
-		let search_input = await page.$("input#popupsearch");
-		await search_input.type(stock);
-		
-		var stock_name = "";
-		var href;
-		// this evaluates to href ,else null
-		href = await page.evaluate(async (stock)=>{
-			let arr = await document.getElementsByClassName('list-group-item text-left');
-		//	console.log(arr);
-			for (let inside of arr){
-				if((inside.innerText).toLowerCase() == stock.toLowerCase()){
-					stock_name = inside.innerText
-					return inside.firstChild.href;
-				}
+		if (stock.length > 3){
+			// pop_search box
+			try{
+				await page.waitForNavigation({timeout: 2000});
+			}catch(error){
+				//console.log(error);\
 			}
-			return null;
-		},stock)
-		// href 
-		if (href !== null){
-			// selection through href
-			var href_click = await page.waitForSelector(`a[href="${href}"]`,{visible: true});
-			if (!href_click){
-				// selection through stock_name
-				href_click = await page.waitForXPath(`//a[contains(text(),"${stock_name}")]`, {visible: true});
+			try{	
+				await page.waitForSelector('#stocksearch-icon',{timeout:3000})
+				await page.click('#stocksearch-icon');
+			}catch (error){
+			//	console.log(error);
+				await page.click('#header-search-box');
 			}
-			await href_click.click();
-			await page.waitForNavigation();
-
-		}else{
-			// alternative is directly searches the stock and nav through their
-
-			let quote_search = await page.waitForSelector('button#quote_search');
-		//	console.log(await quote_search.isVisible());
-			await page.evaluate(()=>{
-				let js = document.getElementById('quote_search');
-				js.click()
-			});
-
-			await page.waitForSelector('#full-search-list');
+			await page.waitForSelector('input#popupsearch');
+			let search_input = await page.$("input#popupsearch");
+			await search_input.type(stock);
 			
-			let match = await page.evaluate(async (stock)=>{
-				
-				let str = document.getElementById('full-search-list');
-				for ( let name of str.innerText.split('\n')){
-					if (stock.toLowerCase().includes(name.toLowerCase())){
-						stock_name = name;
-						return str.innerHTML;
+			var stock_name = "";
+			var href;
+			// this evaluates to href ,else null
+			href = await page.evaluate(async (stock)=>{
+				let arr = await document.getElementsByClassName('list-group-item text-left');
+			//	console.log(arr);
+				for (let inside of arr){
+					if((inside.innerText).toLowerCase() == stock.toLowerCase()){
+						stock_name = inside.innerText
+						return inside.firstChild.href;
 					}
 				}
-				//string here.
-				stock_name = stock;
-				return str.innerHTML
+				return null;
 			},stock)
-			for (let li of match.split('<hr>')){
-				//checks for stock name presence
-				if ((li.toLowerCase()).includes(stock_name.toLowerCase())){
-					href = li.slice(li.indexOf('/'),li.lastIndexOf('"'));
-					var href_click = await page.waitForSelector(`a[href="${href}"]`,{visible: true});
-
-					if (!(href_click)){
-						href_click = await page.waitForXPath(`//a[contains(text(),"${stock_name}")]`, {visible: true});
-					}
-					await href_click.click();
-					break;
-					
+			// href 
+			if (href !== null){
+				// selection through href
+				var href_click = await page.waitForSelector(`a[href="${href}"]`,{visible: true});
+				if (!href_click){
+					// selection through stock_name
+					href_click = await page.waitForXPath(`//a[contains(text(),"${stock_name}")]`, {visible: true});
 				}
-			}
-			if (!(href)){
-				console.log('not found');
-			}else{
+				await href_click.click();
 				await page.waitForNavigation();
-				let checkFor = await page.$('div[class="card-body text-center"]');
-				if (checkFor){
-					let data = await page.evaluate(()=>{
-						let sent_array = [];
-						for (let dataIn of document.querySelectorAll('div[class="card-body text-center"]')){
-							let inside = [];
-							for (let x of dataIn.innerText.split('\n')){
-								if (x == ''|| x==' ' ){
-									continue;
+
+			}else{
+				// alternative is directly searches the stock and nav through their
+
+				let quote_search = await page.waitForSelector('button#quote_search');
+			//	console.log(await quote_search.isVisible());
+				await page.evaluate(()=>{
+					let js = document.getElementById('quote_search');
+					js.click()
+				});
+
+				await page.waitForSelector('#full-search-list');
+				
+				let match = await page.evaluate(async (stock)=>{
+					
+					let str = document.getElementById('full-search-list');
+					for ( let name of str.innerText.split('\n')){
+						if (stock.toLowerCase().includes(name.toLowerCase())){
+							stock_name = name;
+							return str.innerHTML;
+						}
+					}
+					//string here.
+					stock_name = stock;
+					return str.innerHTML
+				},stock)
+				for (let li of match.split('<hr>')){
+					//checks for stock name presence
+					if ((li.toLowerCase()).includes(stock_name.toLowerCase())){
+						href = li.slice(li.indexOf('/'),li.lastIndexOf('"'));
+						var href_click = await page.waitForSelector(`a[href="${href}"]`,{visible: true});
+
+						if (!(href_click)){
+							href_click = await page.waitForXPath(`//a[contains(text(),"${stock_name}")]`, {visible: true});
+						}
+						await href_click.click();
+						break;
+						
+					}
+				}
+				if (!(href)){
+					console.log('not found');
+				}else{
+					await page.waitForNavigation();
+					let checkFor = await page.$('div[class="card-body text-center"]');
+					if (checkFor){
+						let data = await page.evaluate(()=>{
+							let sent_array = [];
+							for (let dataIn of document.querySelectorAll('div[class="card-body text-center"]')){
+								let inside = [];
+								for (let x of dataIn.innerText.split('\n')){
+									if (x == ''|| x==' ' ){
+										continue;
+									}
+									inside.push(x);
+
 								}
-								inside.push(x);
+								sent_array.push(inside);
 
 							}
-							sent_array.push(inside);
+							return sent_array;
+						});
+						data.unshift([stock]);	
+						// Function to convert 2D array to CSV format
+						// Convert your data to CSV format
+						const csvData = convertToCSV(data);
+					
+						// Write the CSV data to a text file
+						fs.appendFileSync(`${formattedDate}-data.txt`, csvData+'', 'utf8', (err) => {
+								if (err) {
+							console.error('Error writing to file:', err);
+								} else {
+							console.log('Data written to file successfully.');
+								}
+						})
 
-						}
-						return sent_array;
-					});
-					data.unshift(['\n\n----',stock,'-----']);	
-					// Function to convert 2D array to CSV format
-					// Convert your data to CSV format
-					const csvData = convertToCSV(data);
-
-					// Write the CSV data to a text file
-					fs.appendFileSync('data.csv', csvData+'\n\n', 'utf8', (err) => {
-					    if (err) {
-						console.error('Error writing to file:', err);
-					    } else {
-						console.log('Data written to file successfully.');
-					    }
-					})
-
+					}
+					
 				}
-				
+
+
 			}
-
-
-		}
-				
+		}			
 	}
 	bar1.stop();
+	await browser.close()
 	
 	
 })();
